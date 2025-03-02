984ee0ffb5956b840d02b329caca6c28d4fcb077
Антон <antonnechlopochin@mail.ru>
2025-03-02 20:36:23 +0300

    Final programm

--- Program.cs
+++ Program.cs
@@ -2,92 +2,117 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Metadata;
-using System.Text.Json;
 using Newtonsoft.Json;
 
-namespace Base_KOD;
+//Чтобы было меньше вопросов, и чтобы работу было проще проверить и понять, прописываю комментарии
 
-
-public class Student{
-    public int ID{get; set;}
-    public string Name{get; set;}
-    public int Points{get; set;}
-
-    public Student( string name, int points){
-        Name = name;
-        Points = points;
-    }
-
-    public void SetId(int id){
-
-    }
-
-
-
-    public override string ToString()
-    {
-        return $"|{ID}| {Name} {Points}";
-    }
-
+public class Student // Данные каждого ученика
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public int Rating { get; set; }
 }
 
+public class StudentService //класс в котором прописана логика добавления/просмотра и сортировки рейтинга учеников
+{
+    private string _filePath; // путь до json - файла с рейтингом
+    private List<Student> _students; //в этой коллекции будет прописана основная информация об учениках
+
+    public StudentService(string filePath)
+    {
+        _filePath = filePath;
+        LoadData();
+    }
+
+    private void LoadData() //метод в котором проверяется есть ли файл в системе, если нет, то создаёт новый
+    {
+        _students = File.Exists(_filePath) ? JsonConvert.DeserializeObject<List<Student>>(File.ReadAllText(_filePath)) ?? new List<Student>() : new List<Student>(); //для удобства и компактности использовал linq запросы
+    }
+
+    public void SaveData() //После изменения коллекции сохраняет данные в json файл
+    {
+        File.WriteAllText(_filePath, JsonConvert.SerializeObject(_students, Formatting.Indented));
+    }
+
+    public void DisplayStudents() // Выводим каждый объект(ученика) в консоль
+    {
+        foreach (var student in _students)
+            Console.WriteLine($"ID: {student.Id}, ФИО: {student.FullName}, Рейтинг: {student.Rating}");
+    }
+
+    public void SortByRating(bool ascending = true) // Сортируем рейтинг как по уменьшении так и по возрастании
+    {
+        _students = ascending ? _students.OrderBy(s => s.Rating).ToList() : _students.OrderByDescending(s => s.Rating).ToList();
+        SaveData();
+    }
+
+    public void SortByName() // Сортируем рейтинг по фамилии
+    {
+        _students = _students.OrderBy(s => s.FullName).ToList();
+        SaveData();
+        
+    }
+
+    public void AddStudent(Student student) // добавляем в коллекцией объект(ученика)
+    {
+        _students.Add(student);
+        SaveData();
+    }
+}
+    
 
 class Program
 {
-    public static void Menu(){
-        bool OpenClose = true;
-        while(OpenClose){
+    private static StudentService _studentService = new StudentService("students.json"); // Название json файла
+
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    private static void Menu()
+    {
+        while (true)
+        {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Меню команд");
+            Console.WriteLine("Меню команд"); //Меню команд
             Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine("1. Просмотреть рейтинговый лист");
-            Console.WriteLine("2. Добавить нового участника");
-            Console.WriteLine("3. Удалить участника");
-            Console.WriteLine("4. Сортировать рейтинг по баллу");
-            Console.WriteLine("5. Сортировать рейтинг по ФИО");
+            Console.WriteLine("1. Просмотреть рейтинговый лист.");
+            Console.WriteLine("2. Добавить нового участника.");
+            Console.WriteLine("3. Сортировать по баллу в порядке уменьш.");
+            Console.WriteLine("4. Сортировать по баллу в порядке возр.");
+            Console.WriteLine("5. Сортировать по ФИО.");
             Console.WriteLine("0. Выйти");
-            Console.WriteLine();
-            Console.Write("Введите номер команды: ");
-            string firstChoiceSTR = Console.ReadLine();
-            int firstChoice = 0;
-            try{
-                firstChoice = int.Parse(firstChoiceSTR);
-            }
-            catch(FormatException){
-                Console.WriteLine("Нет такой команды");
-            }
-            switch (firstChoice){
-                case 0:
-                    OpenClose = false;
-                    break;
+            Console.Write("Выберите команду: ");
 
-                case 1:
-                var everyStudent = ReadAllFromRating();
-                    foreach(var stud in everyStudent){
-                        Console.WriteLine(stud);
-                    }
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "0": 
+                    return;
+                case "1":
+                    ShowStudents();
                     break;
-                case 2:
-                    Console.Write("Введите ФИО: ");
-                    string Fio = Console.ReadLine();
-                    Console.WriteLine("Введите баллы: ");
-                    int points = Convert.ToInt32(Console.ReadLine());
-                    Student newStudent = new Student(Fio, points); 
-                    AddToRating(newStudent);
+                case "2": 
+                    AddStudent(); 
                     break;
-                case 3:
+                case "3":
+                    SortingRatingMin();
                     break;
-                case 4:
+                case "4":
+                    SortingRatingMax();
                     break;
-                case 5:
+                case "5":
+                    SortingNames(); 
                     break;
-                default:
+                case "":
+                    break;
+                default: // Если пользватель введёт не ту команду, то просим ввести команду заново
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine();
-                    Console.WriteLine("Введите корректную команду");
+                    Console.WriteLine("Введите корректную команду!");
                     Console.ResetColor();
                     Console.WriteLine("Чтобы продолжить, нажмите любую клавишу...");
                     Console.ReadKey();
@@ -97,44 +122,87 @@ class Program
         }
     }
 
-    public void AddToRating(Student student){
-        List<Student> sudents = ReadAllFromRating();
-
-        int LastId = sudents.Last().ID;
-
-        student.SetId(LastId + 1);
-
-        sudents.Add(student);
-
-        string serialStudents = JsonConvert.SerializeObject(sudents);
-        File.WriteAllText(DBFilePath, serialStudents);
+    private static void ShowStudents() //Для удобства прописываем методы класса в отдельные функции. 
+                                       // Так при дальнейшей разработке будет проще
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Рейтинг учеников проекта");
+        Console.ResetColor();
+        Console.WriteLine();
+        _studentService.DisplayStudents();
+        Console.ReadKey();
     }
 
-    public void AddToRating(List<Student> students){
-        string serialStudents = JsonConvert.SerializeObject(students);
-        File.WriteAllText(DBFilePath, serialStudents);
+    private static void SortingRatingMin()
+    {
+        _studentService.SortByRating(false);
+        FinishedSort();
+    }
+    private static void SortingRatingMax()
+    {
+        _studentService.SortByRating(true);
+        FinishedSort();
     }
 
-    public void DeleteStudent(int id){
-        List<Student> sudents = ReadAllFromRating();
-        Student StudForDel = sudents.FirstOrDefault(u => u.ID == id);
-        if (StudForDel != null){
-            sudents.Remove(StudForDel);
+    private static void SortingNames()
+    {
+        _studentService.SortByName();
+        FinishedSort(); 
+    }
+
+    private static void AddStudent()
+    {
+        try
+        {
+            Console.Clear();
+            Console.Write("Введите ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("ID");
+            Console.ResetColor();
+            Console.Write(": ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("ФИО");
+            Console.ResetColor();
+            Console.Write(": ");
+            string name = Console.ReadLine();
+            
+            Console.Write("Введите ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("рейтинговый балл");
+            Console.ResetColor();
+            Console.Write(": ");
+            int rating = Convert.ToInt32(Console.ReadLine());
+
+            _studentService.AddStudent(new Student { Id = id, FullName = name, Rating = rating });
+        }
+        catch (Exception)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ошибка ввода данных!");
+            Console.ResetColor();
         }
     }
 
-    public static List<Student> ReadAllFromRating(){
-        string JSon = File.ReadAllText(DBFilePath);
-        List<Student> currStudents = JsonConvert.DeserializeObject<List<Student>>(JSon);
-        return currStudents;
-    }
-    static string DBFilePath{get; set;}
-    static void Main(string[] args)
+    private static void ShowError() 
     {
-        string fileDBname = "rating list";
-        string FileFolderPath = Path.GetTempPath();
-        DBFilePath = FileFolderPath + fileDBname;
-        Console.WriteLine(FileFolderPath);
-        Menu();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Ошибка! Проверьте вводимые данные.");
+        Console.ResetColor();
+        Console.WriteLine("Нажмите любую клавишу...");
+        Console.ReadKey();
+    }
+
+    private static void FinishedSort()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Сортировка выполнена!");
+        Console.ResetColor();
+        Console.WriteLine("Нажмите любую клавишу...");
+        Console.ReadKey();
     }
 }
